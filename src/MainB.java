5fb059cd8951e6c7a31471886b999df160416255
blackaichi <eric.casanovas@est.fib.upc.edu>
2019-05-25 14:58:49 +0200

    Fet jar

--- src/MainB.java
+++ src/MainB.java
@@ -4,23 +4,20 @@ import org.knowm.xchart.XYChartBuilder;
 import org.knowm.xchart.XYSeries;
 import org.knowm.xchart.style.XYStyler;
 import org.knowm.xchart.style.colors.XChartSeriesColors;
-
 import java.util.Scanner;
 
-class ApartatB {
-    private final double indexFibra = 1.47;
-    private int regio = 0;
-    private int nreg;
-    private double angleF = 0;
-    private boolean up = true;
-    private double posX;
-    private double posY;
-    private int alpha;
-    private int pointsxU;
+class MainB {
+    private static final double indexFibra = 1.47;
+    private static int regio = 0;
+    private static int nreg;
+    private static double angleF = 0;
+    private static boolean up = true;
+    private static double posX;
+    private static double posY;
+    private static int alpha;
+    private static int pointsxU;
 
-    ApartatB() {}
-
-    void main() throws Exception {
+    public static void main(String[] args) throws Exception {
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter Natural Number of Regions (from 20 to 50): ");
         nreg = reader.nextInt();
@@ -98,7 +95,7 @@ class ApartatB {
         }
     }
 
-    private double[][] getData(int mode) {
+    private static double[][] getData(int mode) {
         double[] xData;
         double[] yData;
         if (mode == 0) {
@@ -142,7 +139,7 @@ class ApartatB {
         return new double[][] { xData, yData };
     }
 
-    private void downThings(double[] xData, double[] yData, int i) {
+    private static void downThings(double[] xData, double[] yData, int i) {
         yData[i] = posY - 1;
         xData[i] = posX + (Math.tan(Math.PI * angleF / 180));
         regio--;
@@ -151,7 +148,7 @@ class ApartatB {
 
     }
 
-    private void upThings(double[] xData, double[] yData, int i) {
+    private static void upThings(double[] xData, double[] yData, int i) {
         yData[i] = posY + 1;
         xData[i] = posX + (Math.tan(Math.PI * angleF / 180));
         regio++;
@@ -159,17 +156,17 @@ class ApartatB {
         posX = posX + (Math.tan(Math.PI * angleF / 180));
     }
 
-    private double calculIndexRegio(double r, double a, int alfa) {
+    private static double calculIndexRegio(double r, double a, int alfa) {
         double delta = 0.01;
         return indexFibra*Math.sqrt(1-2*delta*Math.pow((Math.abs(r)/a),alfa));
     }
 
-    private double snell(double n1, double angleIncident, double n2) {
+    private static double snell(double n1, double angleIncident, double n2) {
         //System.out.println("n1 = " + n1 + ", angI = " + angleIncident + ", n2 = " + n2);
         return Math.asin(n1*Math.sin(Math.PI*angleIncident/180)/n2)*180/Math.PI;
     }
 
-    private double angleCritic(double n1, double n2) {
+    private static double angleCritic(double n1, double n2) {
         if (n1 < n2) return Math.asin(n1/n2)*180/Math.PI;
         return Math.asin(n2/n1)*180/Math.PI;
     }
